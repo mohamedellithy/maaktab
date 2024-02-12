c1890c694bdae1fdf6ec7c17cde95014364569f4
mohamedellithy <develomohamed22@gmail.com>
2024-02-12 21:25:29 +0200

    d

--- resources/views/pages/front/index.blade.php
+++ resources/views/pages/front/index.blade.php
@@ -230,15 +230,12 @@ $page = ActivePagesMenus(['slug','=','/'])[0];
 
     <!-- ====== Contact Start ====== -->
     @if(isset($page->content['contact_banner']['enable']) && $page->content['contact_banner']['enable'] == 'active')
-        <section id="contact " class="ud-contact ">
+        <section id="contact" class="ud-contact ">
             <div class="container ">
                 <div class="row align-items-center ">
                     <div class="col-xl-8 col-lg-7 ">
                         <div class="ud-contact-content-wrapper ">
                             <div class="ud-contact-title ">
-                                <span>
-                                    اطلب خدمتك
-                                </span>
                                 <h2>
                                     {{ isset($page->content['contact_banner']['heading']) ? $page->content['contact_banner']['heading'] : 'قم بتواصل معنا' }}
                                 </h2>
